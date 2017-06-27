3c526f891c32c639a42ef515198d9c7cd085371a
KITRIGAME-PC\KITRIGAME <kiege86@gmail.com>
2017-06-27 16:59:09 +0900

    3인칭 카메라

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -38,13 +38,13 @@ public class CameraManager : MonoSingleton<CameraManager>
 		//currentHeight = Mathf.Lerp(currentHeight, wantedHeight, HeightDamping * Time.deltaTime);
 		//currentWidth = Mathf.Lerp(currentWidth, wantedWidth, WidthDamping * Time.deltaTime);
 
-		Vector3 pos = Target.position;
+		//Vector3 pos = Target.position;
 		//pos -= MainCamera.transform.forward * Distance;
 
 		//MainCamera.transform.position = new Vector3(currentWidth, currentHeight, pos.z);
 
-		MainCamera.transform.position = pos + new Vector3(0, 1.5f, -1.5f);
-		MainCamera.transform.LookAt(Target);
-		MainCamera.transform.rotation = Quaternion.Euler(new Vector3(25.0f, 0, 0));
+		//MainCamera.transform.position = pos + new Vector3(0, 1.5f, -1.5f);
+		//MainCamera.transform.LookAt(Target);
+		//MainCamera.transform.rotation = Quaternion.Euler(new Vector3(25.0f, 0, 0));
 	}
 }
