52fd23f1704dc24410a5800a3cdb50b4c96dc6b0
KITRIGAME-PC\KITRIGAME <37ghals@naver.com>
2017-06-29 18:00:47 +0900

    6/29인게임UI

--- Assets/Scripts/UI/UI_Option_Game.cs
+++ Assets/Scripts/UI/UI_Option_Game.cs
@@ -4,13 +4,97 @@ using UnityEngine;
 
 public class UI_Option_Game : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	UIButton CloseBtn = null;
+	UIButton GameOutBtn = null;
+
+	Transform trans = null;
+
+	UIProgressBar BgmPro = null; //배경음
+	UIButton BgmPlus = null;
+	UIButton BgmMinus = null;
+
+	UIProgressBar SoundPro = null; //효과음
+	UIButton SoundPlus = null;
+	UIButton SoundMinus = null;
+
+	private void Awake()
+	{
+		CloseBtn = transform.FindChild("BackGround").FindChild("Top").FindChild("Close").GetComponent<UIButton>();
+		if (CloseBtn == null)
+			Debug.Log("CloseBtn is null");
+		EventDelegate.Add(CloseBtn.onClick, new EventDelegate(this, "ClosePanel")); //닫기버튼
+
+		GameOutBtn = transform.FindChild("BackGround").FindChild("GameOutBtn").GetComponentInChildren<UIButton>();
+		if (GameOutBtn == null)
+			Debug.Log("GameOutBtn is null");
+		EventDelegate.Add(GameOutBtn.onClick, new EventDelegate(this, "GoLobby")); // 게임포기 버튼
+
 		
+
+		BgmPro = transform.FindChild("BackGround").FindChild("BGM").FindChild("Progress").GetComponent<UIProgressBar>();
+		if (BgmPro == null)
+			Debug.Log("BgmPro is null");
+
+		BgmPlus = transform.FindChild("BackGround").FindChild("BGM").FindChild("Plus").GetComponent<UIButton>();
+		if (BgmPlus == null)
+			Debug.Log("BgmPlus is null");
+		EventDelegate.Add(BgmPlus.onClick, new EventDelegate(this, "PlusBGM")); // BGM+ 버튼
+
+		BgmMinus = transform.FindChild("BackGround").FindChild("BGM").FindChild("Minus").GetComponent<UIButton>();
+		if (BgmMinus == null)
+			Debug.Log("BgmMinus is null");
+		EventDelegate.Add(BgmMinus.onClick, new EventDelegate(this, "MinusBGM")); // BGM- 버튼
+
+
+		SoundPro = transform.FindChild("BackGround").FindChild("Sound").FindChild("Progress").GetComponent<UIProgressBar>();
+		if (SoundPro == null)
+			Debug.Log("SoundPro is null");
+
+		SoundPlus = transform.FindChild("BackGround").FindChild("Sound").FindChild("Plus").GetComponent<UIButton>();
+		if (SoundPlus == null)
+			Debug.Log("SoundPlus is null");
+		EventDelegate.Add(SoundPlus.onClick, new EventDelegate(this, "PlusSound")); // Sound+ 버튼
+
+		SoundMinus = transform.FindChild("BackGround").FindChild("Sound").FindChild("Minus").GetComponent<UIButton>();
+		if (SoundMinus == null)
+			Debug.Log("SoundMinus is null");
+		EventDelegate.Add(SoundMinus.onClick, new EventDelegate(this, "MinusSound")); // Sound- 버튼
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void ClosePanel() //닫기버튼클릭
+	{
+		Destroy(this.gameObject);
+		//gameObject.SetActive(false);
+		Debug.Log("닫기 클릭");
+	}
+
+	void PlusBGM() //배경음 볼륨조절
+	{
+		BgmPro.value += 0.1f;
+		Debug.Log("bgm 볼륨상승");
+	}
+
+	void MinusBGM()
+	{
+		BgmPro.value -= 0.1f;
+		Debug.Log("bgm 볼륨감소");
+	}
+
+	void PlusSound() //효과음 볼륨조절
+	{
+		SoundPro.value += 0.1f;
+		Debug.Log("sound 볼륨상승");
+	}
+
+	void MinusSound()
+	{
+		SoundPro.value -= 0.1f;
+		Debug.Log("sound 볼륨감소");
+	}
+
+	void GoLobby()
+	{
+		//UI_Popup
+		Scene_Manager.Instance.LoadScene(eSceneType.SCENE_LOBBY);
 	}
 }
---
+++ Assets/UI_Game.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Game : MonoBehaviour {
+
+	UIButton OptionBtn = null;
+	UIButton JumpBtn = null;
+	UIButton SlideBtn = null;
+	UIButton MapBtn = null;
+	UIButton MapCloseBtn = null;
+
+	GameObject MapPrefab;
+
+	private void Awake()
+	{
+		MapPrefab = Resources.Load("Prefabs/UI/PF_UI_MiniMap") as GameObject;
+
+		OptionBtn =	transform.FindChild("Top").FindChild("Option").FindChild("Btn").GetComponent<UIButton>();
+		if (OptionBtn == null)
+			Debug.Log("OptionBtn is null");
+
+		EventDelegate.Add(OptionBtn.onClick, new EventDelegate(this,"ShowOption"));
+
+		JumpBtn = transform.FindChild("BackGround").FindChild("JumpBtn").GetComponent<UIButton>();
+		if (JumpBtn == null)
+			Debug.Log("JumpBtn is null");
+
+		EventDelegate.Add(JumpBtn.onClick, new EventDelegate(this, "PlayerJump"));
+
+		SlideBtn = transform.FindChild("BackGround").FindChild("SlideBtn").GetComponent<UIButton>();
+		if (SlideBtn == null)
+			Debug.Log("SlideBtn is null");
+
+		EventDelegate.Add(SlideBtn.onClick, new EventDelegate(this, "PlayerSlide"));
+
+		MapBtn = transform.FindChild("BackGround").FindChild("MiniMapBtn").GetComponent<UIButton>();
+		if (MapBtn == null)
+			Debug.Log("MapBtn is null");
+
+		EventDelegate.Add(MapBtn.onClick, new EventDelegate(this, "ShowMap"));
+
+		MapCloseBtn = transform.parent.FindChild("PF_UI_MiniMap").GetComponent<UIButton>();
+		if (MapCloseBtn == null)
+			Debug.Log("MapCloseBtn is null");
+
+		EventDelegate.Add(MapCloseBtn.onClick, new EventDelegate(this, "CloseMap"));
+
+	}
+
+	void ShowOption()
+	{
+		GameObject OptionPrefab;
+		OptionPrefab = Resources.Load("Prefabs/UI/PF_UI_OptionPanel_InGame") as GameObject;
+		NGUITools.AddChild(gameObject.transform.parent.gameObject, OptionPrefab);
+		Debug.Log("옵션오픈");
+	}
+
+	void PlayerJump()
+	{
+		Debug.Log("점프!");
+	}
+
+	void PlayerSlide()
+	{
+		Debug.Log("슬라이드!");
+	}
+
+	void ShowMap()
+	{
+		
+		NGUITools.AddChild(gameObject.transform.parent.gameObject, MapPrefab);
+	}
+
+	void CloseMap()
+	{
+		Debug.Log("미니맵종료");
+		Destroy(GameObject.Find("PF_UI_MiniMap"));
+	}
+
+	private void Update()
+	{
+
+	}
+
+
+}
