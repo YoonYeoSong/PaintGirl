d67ab4532a7d354bcc03d3083c04eac26f16535b
KITRIGAME-PC\KITRIGAME <37ghals@naver.com>
2017-06-30 18:01:54 +0900

    no message

---
+++ Assets/Scripts/UI/UI_Store.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Store : MonoBehaviour {
+
+	UIButton Gold = null;
+
+	private void Awake()
+	{
+		Gold = transform.FindChild("BackGround").FindChild("ScrollView").FindChild("Grid").FindChild("Store_Gold").FindChild("ScrollView").FindChild("Grid").FindChild("Gold_Little").FindChild("Btn").GetComponent<UIButton>();
+
+		EventDelegate.Add(Gold.onClick, new EventDelegate(this, "ClickGold"));
+	}
+	void ClickGold()
+	{
+		Debug.Log("gold");
+	}
+
+
+}
