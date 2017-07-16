4eb3dcae100f6563366b52b384566496d9db2424
MSDN-SPECIAL\Administrator <37ghals@naver.com>
2017-07-17 02:18:02 +0900

    no message

---
+++ Assets/Scripts/Game/AirPlane.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirPlane : MonoBehaviour {
+
+	float CurTime = 0.0f;
+	bool AirPlaneUpCheck;
+	// Use this for initialization
+	void Start () {
+		AirPlaneUpCheck = false;
+
+		transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+		transform.localPosition = new Vector3(-40, 15.1f, -8.42f);
+		transform.localRotation = Quaternion.Euler(-90, 0, 0);
+		transform.FindChild("bomb_plane 1").localRotation = Quaternion.Euler(0, 20, 0);
+		Destroy(gameObject, 15.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CurTime += Time.deltaTime;
+
+
+
+		//transform.FindChild("bomb_plane 1").localRotation = Quaternion.Euler(0, 20 - (CurTime / 1.0f * 15), 0);
+		
+
+		if (transform.localPosition.x < -22)
+			{
+				AirPlaneDown();
+			}
+			if (0 > transform.localPosition.x && transform.localPosition.x >= -22)
+			{
+
+
+				transform.FindChild("bomb_plane 1").localRotation = Quaternion.Euler(0, 20 - (CurTime / 1.0f * 15), 0);
+				AirPlaneStraight();
+
+				if (transform.FindChild("bomb_plane 1").localRotation.y <= 0)
+				{
+					transform.FindChild("bomb_plane 1").localRotation = Quaternion.Euler(0, 0, 0);
+				}
+			}
+			if (transform.localPosition.x >= 0.0f && AirPlaneUpCheck == false)
+			{
+				CurTime = 0.0f;
+				AirPlaneUpCheck = true;
+			}
+			if (transform.localPosition.x > 0 && AirPlaneUpCheck == true)
+			{
+				if (transform.localPosition.x <= 8.0f)
+				{
+					transform.FindChild("bomb_plane 1").localRotation = Quaternion.Euler(0, 0 - (CurTime / 1.0f * 15), 0);
+
+				}
+
+				AirPlaneUp();
+			}
+		}
+
+	void AirPlaneDown()
+	{
+		Transform temp;
+		CurTime = 0.0f;
+		transform.localPosition += new Vector3(0.1f, -0.05f, 0);
+		transform.FindChild("bomb_plane 1").localRotation = Quaternion.Euler(0, 20 - (CurTime / 2.0f * 15), 0);
+		//temp.localRotation = 30 - (CurTime / 1.0f * 15);
+	}
+
+	void AirPlaneStraight()
+	{
+		transform.localPosition += new Vector3(0.1f, 0, 0);
+
+	}
+
+	void AirPlaneUp()
+	{
+		transform.localPosition += new Vector3(0.1f, 0.02f, 0);
+
+	}
+
+}
--- Assets/Scripts/Game/ItemGenerator.cs
+++ Assets/Scripts/Game/ItemGenerator.cs
@@ -16,7 +16,7 @@ public class ItemGenerator : MonoBehaviour {
 	// Use this for initialization
 
 	GameObject AirPlane = null;
-	bool AirPlaneUpCheck = false;
+	
 	void Start () {
 
         HiddenTimeCheck = GameObject.Find("Timer").GetComponent<GameCountTime>();
@@ -36,49 +36,14 @@ public class ItemGenerator : MonoBehaviour {
 
 		CurTime += Time.deltaTime;
 
-		
-        if (HiddenTimeCheck.HiddenTime == true)
-        {
-            Instantiate(HiddenItem, new Vector3(-8.81f, 3.43f, -1.388f), Quaternion.Euler(0, 180, 0));
-            HiddenTimeCheck.HiddenTime = false;
-        }
-
-		if (AirPlane != null)
+		if (HiddenTimeCheck.HiddenTime == true)
 		{
-			if (AirPlane.transform.localPosition.x < -22)
-			{
-				AirPlaneDown();
-			}
-			if (0 > AirPlane.transform.localPosition.x  && AirPlane.transform.localPosition.x >= -22)
-			{
-				
-
-					AirPlane.transform.FindChild("bomb_plane 1").localRotation = Quaternion.Euler(0, 20 - (CurTime / 1.0f * 15), 0);
-					AirPlaneStraight();
-
-				if (AirPlane.transform.FindChild("bomb_plane 1").localRotation.y <= 0)
-				{ 
-					AirPlane.transform.FindChild("bomb_plane 1").localRotation = Quaternion.Euler(0, 0, 0);
-				}
-			}
-			if (AirPlane.transform.localPosition.x >= 0.0f && AirPlaneUpCheck == false)
-			{
-				CurTime = 0.0f;
-				AirPlaneUpCheck = true;
-			}
-			if (AirPlane.transform.localPosition.x > 0 && AirPlaneUpCheck == true)
-			{
-				if ((0 >= AirPlane.transform.FindChild("bomb_plane 1").localRotation.y) && ( AirPlane.transform.FindChild("bomb_plane 1").localRotation.y >= -10.0f))
-				{
-					AirPlane.transform.FindChild("bomb_plane 1").localRotation = Quaternion.Euler(0, 0 - (CurTime / 1.0f * 15), 0);
-
-				}
-
-				AirPlaneUp();
-			}
+			Instantiate(HiddenItem, new Vector3(-8.81f, 3.43f, -1.388f), Quaternion.Euler(0, 180, 0));
+			HiddenTimeCheck.HiddenTime = false;
 		}
-		
-    }
+
+
+	}
 
 	void ItemRegen()
 	{
@@ -94,32 +59,10 @@ public class ItemGenerator : MonoBehaviour {
 		
 		GameObject temp = Resources.Load("Prefabs/Game/YellowAirPlane") as GameObject;
 		AirPlane = Instantiate(temp, Vector3.zero,Quaternion.identity);
-		AirPlane.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-		AirPlane.transform.localPosition = new Vector3(-40, 15.1f, -8.42f);
-		AirPlane.transform.localRotation = Quaternion.Euler(-90,0,0);
-		AirPlane.transform.FindChild("bomb_plane 1").localRotation = Quaternion.Euler(0, 20, 0);
-	}
-
-
-	void AirPlaneDown()
-	{
-		Transform temp;
-		CurTime = 0.0f;
-		AirPlane.transform.localPosition += new Vector3(0.1f, -0.05f, 0);
-		//AirPlane.transform.FindChild("bomb_plane 1").localRotation = Quaternion.Euler(0, 30 - (CurTime / 1.0f * 15), 0);
-		//temp.localRotation = 30 - (CurTime / 1.0f * 15);
-	}
-
-	void AirPlaneStraight()
-	{
-		AirPlane.transform.localPosition += new Vector3(0.1f, 0, 0);
-
-	}
-
-	void AirPlaneUp()
-	{
-		AirPlane.transform.localPosition += new Vector3(0.1f, 0.02f, 0);
 		
 	}
+
+
+
 
 }
