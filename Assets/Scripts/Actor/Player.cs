c7df9cf5ab3813dd4ba35984b2fc51209513a558
KITRIGAME-PC\KITRIGAME <hooj3@naver.com>
2017-07-26 13:08:31 +0900

    no message

--- Assets/Scripts/Actor/Player.cs
+++ Assets/Scripts/Actor/Player.cs
@@ -370,13 +370,7 @@ public class Player : Actor
 
 	}
 
-	private void OnTriggerEnter(Collider other)
-	{
-		m1 = other.transform.tag;
-		Debug.Log("피이격"+ m1);
 
-
-	}
 
 	void CheckGround()
 	{
@@ -386,7 +380,7 @@ public class Player : Actor
 		if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.9f))
 		{
 			if (hit.transform.tag == "GROUND")
-			{
+			{   
 				grounded = true;
 				return;
 			}
@@ -417,12 +411,12 @@ public class Player : Actor
 			Debug.Log("피격!");
 	}
 
-	private void OnParticleCollision(GameObject other)
-	{
-		m1 = other.tag;
-		if(other.transform.tag == "APlayer")
-			Debug.Log("피격!");
-	}
+	//private void OnParticleCollision(GameObject other)
+	//{
+	//	m1 = other.tag;
+	//	if(other.transform.tag == "APlayer")
+	//		Debug.Log("피격!");
+	//}
 
 
 	public void Buff(int TeamCheck)
